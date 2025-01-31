39775ee783a3fc2fd66dc9186008802aa2a8fdc7
Tanzeem <dev.tanzeem@gmail.com>
2025-01-31 12:33:55 +0530

    cors resolving

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,2 +1,3 @@
-export const BASE_URL = "http://localhost:8080";
+// export const BASE_URL = "http://localhost:8080";
 // export const BASE_URL = "https://assignment-backend-g51m.onrender.com";
+export default BASE_URL = "https://assignment-backend2-0ep8.onrender.com";
